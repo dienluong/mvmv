c08e4e0bd18cf080b746cbae0e8919addfcd85ae
Dien Luong <dysonlu@gmail.com>
2017-04-10 18:34:56 -0400

    Added test for parse()

--- test/mv-parser.spec.js
+++ test/mv-parser.spec.js
@@ -5,7 +5,7 @@ var myParser  = require('../src/mv-parser');
 describe('mv-parser', function () {
     describe('parse()', function () {
         beforeEach(function () {
-            this.parser = Object.create(myParser);
+            this.parser = myParser;
         });
         it('should accept only one * wildcard in glob pattern', function () {
             expect(this.parser.parse.bind(this.parser, 'node mv.js *.* *.jpg')).to.throw(Error);
