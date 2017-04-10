e9c3b1a742aca0638bd9b0fa98654f97eeea52d9
Dien Luong <dysonlu@gmail.com>
2017-04-10 13:10:05 -0400

    Initial commit

---
+++ test/mv.spec.js
@@ -0,0 +1,14 @@
+var expect  = require('chai').expect;
+var sinon   = require('sinon');
+var mv      = require('mv');
+
+describe('mv', function () {
+    describe('parseArgs', function () {
+        beforeEach(function () {
+           this.mv = Object.create(mv);
+        });
+        it('should accept only one * wildcard in glob pattern', function () {
+
+        });
+    });
+});
