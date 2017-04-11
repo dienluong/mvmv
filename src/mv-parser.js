674a8e68bd3d738fffdd942712cef42ffe95a1d4
Dien Luong <dysonlu@gmail.com>
2017-04-11 17:08:18 -0400

    Start implementation of file globbing

--- src/mv-parser.js
+++ src/mv-parser.js
@@ -1,7 +1,12 @@
-var mvParser = {
-    parse: function () {
+var fs = require('fs');
+var glob = require('glob'); //http://stackoverflow.com/a/25580289
 
-    }
-};
+function createParser () {
+    return {
+        resolve: function (patterns) {
+            return glob.sync(patterns);
+        }
+    };
+}
 
-module.exports = mvParser;
+module.exports.create = createParser;
