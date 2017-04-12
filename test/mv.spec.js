487cf6b59ec3c121cbd298d43a117e644ddffb91
Dien Luong <dysonlu@gmail.com>
2017-04-12 13:18:49 -0400

    Added test for default parser during init

--- test/mv.spec.js
+++ test/mv.spec.js
@@ -4,6 +4,25 @@ var Mv      = require('../src/mv');
 var Parser  = require('../src/mv-parser');
 
 describe('mv', function () {
+    describe('init()', function () {
+        beforeEach(function () {
+            this.argv = process.argv;   // backs up argv
+            process.argv = [process.execPath, 'ms.js', '*.txt', '*.jpg'];
+        });
+
+        afterEach(function () {
+            process.argv = this.argv;
+
+        });
+
+        it('should use default parser if none provided', function () {
+            var myMv = Mv.create();
+            myMv.init();
+            myMv.run();
+            expect(myMv.run).to.not.throw(Error);
+        })
+    });
+
     describe('run()', function () {
         beforeEach(function () {
             this.argv = process.argv;   // backs up argv
