896e5e0955ab3ce802fd8ddee3374f624f03cd4c
Dien Luong <dysonlu@gmail.com>
2017-04-12 02:10:01 -0400

    Minor changes

--- test/mv-parser.spec.js
+++ test/mv-parser.spec.js
@@ -3,16 +3,16 @@ var expect  = require('chai').expect;
 var sinon   = require('sinon');
 var myParser  = require('../src/mv-parser');
 
-const txtFileNames = [ 'test-data/4911339-know-sudan-2017-04-11-drama.txt',
-    'test-data/burundi_such_sm_help.txt',
-    'test-data/ethiopia-492795-military-buenos-aires.txt',
-    'test-data/faq-pretoria-moreover.txt',
-    'test-data/guides_small_guinea_2017-04-11_astana.txt',
-    'test-data/italy-jobs-wipe.txt',
-    'test-data/marshall-islands_billy-preston_bottom_17.txt',
-    'test-data/religion-nigeria.txt',
-    'test-data/sports-divine-8499096-putrajaya.txt',
-    'test-data/turkey-2784-drama.txt'
+const txtFileNames = [ 'test/test-data/4911339-know-sudan-2017-04-11-drama.txt',
+    'test/test-data/burundi_such_sm_help.txt',
+    'test/test-data/ethiopia-492795-military-buenos-aires.txt',
+    'test/test-data/faq-pretoria-moreover.txt',
+    'test/test-data/guides_small_guinea_2017-04-11_astana.txt',
+    'test/test-data/italy-jobs-wipe.txt',
+    'test/test-data/marshall-islands_billy-preston_bottom_17.txt',
+    'test/test-data/religion-nigeria.txt',
+    'test/test-data/sports-divine-8499096-putrajaya.txt',
+    'test/test-data/turkey-2784-drama.txt'
 ];
 
 describe('mv-parser', function () {
@@ -29,13 +29,11 @@ describe('mv-parser', function () {
 
         it('should return [] when none matched', function () {
             var fileList = this.parser.resolve(path.join('test-data', '*.bob'));
-            console.log(fileList);
             expect(this.parser.resolve.returned([])).to.be.true;
         });
 
         it('should return array of file names matching glob pattern', function () {
-            var fileList = this.parser.resolve(path.join('test-data', '*.txt'));
-            console.log(fileList);
+            var fileList = this.parser.resolve(path.join('test', 'test-data', '*.txt'));
             expect(this.parser.resolve.returned(txtFileNames)).to.be.true;
         });
     });
