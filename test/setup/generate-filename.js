25e9cb2b7a73a7995507794de4b547dc21ca2744
Dien Luong <dysonlu@gmail.com>
2017-04-12 16:56:22 -0400

    Minor changes

--- test/setup/generate-filename.js
+++ test/setup/generate-filename.js
@@ -3,5 +3,5 @@ var filenameGen = require('natural-filename-generator');
 var g = new filenameGen();
 
 for (let i = 0; i < 10; i++) {
-    console.log(g.generate('txt'));
+    console.log(g.generate(''));
 }
