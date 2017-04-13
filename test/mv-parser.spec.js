a41a765859d5db272f3a2432d4f8a3e1e2d932b3
Dien Luong <dysonlu@gmail.com>
2017-04-13 01:24:04 -0400

    Updated test cases

--- test/mv-parser.spec.js
+++ test/mv-parser.spec.js
@@ -2,12 +2,30 @@ var mockFs  = require('mock-fs');
 var filenameGen = require('natural-filename-generator');
 var globby  = require('globby');
 var path    = require('path');
+
 var expect  = require('chai').expect;
 var sinon   = require('sinon');
-var myParser  = require('../src/mv-parser');
+
+var Parser  = require('../src/mv-parser');
 
 describe('mv-parser', function () {
     describe('resolve()', function () {
+        it('should return [] when none matched', function () {
+            let pattern = path.join('test', 'test-data', '*.bob');
+            let result = this.parser.resolve(pattern);
+
+            expect(this.parser.resolve.returned([])).to.be.true;
+            expect(result).to.eql(globby.sync(pattern));
+        });
+
+        it('should return array of file names matching glob pattern', function () {
+            let globPattern = path.join('test', 'test-data', '*.txt');
+            let txtFileNames = globby.sync(globPattern);
+
+            let result = this.parser.resolve(globPattern);
+            expect(result).to.eql(txtFileNames);
+        });
+
         before(function () {
             var g = new filenameGen();
 
@@ -18,47 +36,33 @@ describe('mv-parser', function () {
                     [g.generate('txt')] : 'created by mock-fs',
                     [g.generate('txt')] : 'created by mock-fs',
                     [g.generate('TXT')] : 'created by mock-fs',
-                    [g.generate('jpg')] : 'created by mock-fs',
+                    [g.generate('TXT')] : 'created by mock-fs',
+                    [g.generate('TXT')] : 'created by mock-fs',
                     [g.generate('jpg')] : 'created by mock-fs',
                     [g.generate('JPG')] : 'created by mock-fs',
                     [g.generate('jpeg')] : 'created by mock-fs',
-                    [g.generate('jpeg')] : 'created by mock-fs',
-                    [g.generate('png')] : 'created by mock-fs',
-                    [g.generate('png')] : 'created by mock-fs',
-                    [g.generate('gif')] : 'created by mock-fs',
-                    [g.generate('gif')] : 'created by mock-fs',
                     [g.generate('JPEG')] : 'created by mock-fs',
+                    [g.generate('png')] : 'created by mock-fs',
+                    [g.generate('PNG')] : 'created by mock-fs',
+                    [g.generate('gif')] : 'created by mock-fs',
                     [g.generate('GIF')] : 'created by mock-fs',
                     [g.generate('')] : 'created by mock-fs'
                 }
             });
-
-            this.globPattern = path.join('test', 'test-data', '*.txt');
-            this.txtFileNames = globby.sync(this.globPattern);
         });
 
         after(function () {
-           mockFs.restore();
+            mockFs.restore();
         });
 
         beforeEach(function () {
-            this.parser = myParser.create();
+            this.parser = Parser.create();
             sinon.spy(this.parser, 'resolve');
 
         });
 
         afterEach(function () {
             this.parser.resolve.restore();
-        });
-
-        it('should return [] when none matched', function () {
-            this.parser.resolve(path.join('test-data', '*.bob'));
-            expect(this.parser.resolve.returned([])).to.be.true;
-        });
-
-        it('should return array of file names matching glob pattern', function () {
-            var fileList = this.parser.resolve(this.globPattern);
-            expect(fileList).to.eql(this.txtFileNames);
         });
     });
 });
