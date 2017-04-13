f55d80b291c82c16f07f88c8f44ab5e721af4d14
Dien Luong <dysonlu@gmail.com>
2017-04-12 22:22:12 -0400

    Added test for mover.mover() call

--- test/mv.spec.js
+++ test/mv.spec.js
@@ -1,7 +1,12 @@
+// Mock fs core module
+var mockFs = require('mock-fs');
+
 var expect  = require('chai').expect;
 var sinon   = require('sinon');
+
 var Mv      = require('../src/mv');
 var Parser  = require('../src/mv-parser');
+var Mover   = require('../src/mv-mover');
 
 describe('mv', function () {
     describe('init()', function () {
@@ -12,11 +17,10 @@ describe('mv', function () {
 
         afterEach(function () {
             process.argv = this.argv;
-
         });
 
         it('should use default parser if none provided', function () {
-            var myMv = Mv.create();
+            let myMv = Mv.create();
             myMv.init();
             myMv.run();
             expect(myMv.run).to.not.throw(Error);
@@ -27,20 +31,30 @@ describe('mv', function () {
         beforeEach(function () {
             this.argv = process.argv;   // backs up argv
             process.argv = [process.execPath, 'ms.js', '*.txt', '*.jpg'];
+
             this.myParser = Parser.create();
-            sinon.spy(this.myParser, "resolve");
+            this.myMover = Mover.create();
             this.mv = Mv.create();
-            this.mv.init(this.myParser);
+            this.mv.init(this.myParser, this.myMover);
+
+            sinon.spy(this.myMover, "move");
+            sinon.spy(this.myParser, "resolve");
         });
 
         afterEach(function () {
            this.myParser.resolve.restore();
+           this.myMover.move.restore();
            process.argv = this.argv;
         });
 
-        it('should call parser.resolve() with glob patterns from command line', function () {
+        it('should call parser.resolve() with 1st glob pattern from command line', function () {
             this.mv.run();
             expect(this.myParser.resolve.calledWith('*.txt')).to.be.true;
         });
+
+        it('should call mover.move() with 2nd glob pattern from command line', function () {
+            this.mv.run();
+            expect(this.myMover.move.calledWith('*.jpg')).to.be.true;
+        })
     });
 });
