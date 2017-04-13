e587f05f54ff2a54a4723e5e487ce917e7eace25
Dien Luong <dysonlu@gmail.com>
2017-04-13 01:21:41 -0400

    Minor changes

--- src/mv-parser.js
+++ src/mv-parser.js
@@ -4,7 +4,10 @@ var glob = require('glob');
 function createParser () {
     return {
         resolve: function (patterns) {
-            return glob.sync(patterns);
+            //returns array of string for the matches, or empty array if no match
+            var results = glob.sync(patterns, {noglobstar: true, nodir: true});
+            console.log(results);
+            return results;
         }
     };
 }
