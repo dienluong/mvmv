b82a050f2339ca27076662cd678cd4081ab2cad8
Dien Luong <dysonlu@gmail.com>
2017-04-14 16:03:22 -0400

    Using additional glob settings

--- src/mv-parser.js
+++ src/mv-parser.js
@@ -2,10 +2,17 @@
 var glob = require('glob');
 
 function createParser () {
+    const opts = {
+        nobrace: true,
+        noglobstar: true,
+        noext: true,
+        nodir: true
+    };
+
     return {
         resolve: function (patterns) {
             //returns array of string for the matches, or empty array if no match
-            var results = glob.sync(patterns, {noglobstar: true, nodir: true});
+            var results = glob.sync(patterns, opts);
             console.log(results);
             return results;
         }
