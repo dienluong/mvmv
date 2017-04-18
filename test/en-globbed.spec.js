7146b725d0194342e17368417df25e4c9385b39a
Dien Luong <dysonlu@gmail.com>
2017-04-17 22:49:46 -0400

    First test case added

--- test/en-globbed.spec.js
+++ test/en-globbed.spec.js
@@ -1,8 +1,12 @@
 var expect    = require('chai').expect;
 var sinon   = require('sinon');
 
-var Mover   = require('../src/en-globbed');
+var englobbed   = require('../src/en-globbed');
 
 describe('englobbed', function () {
+    it('should return \"homer\" for wildcard *, when called w/ (path=[\"homer.js\"], glob=\"*.js\")', function () {
+        let result = englobbed(['homer.js'], '*.js');
 
+        expect(result[0]).to.eql([{wildcard: '*', match: 'homer'}]);
+    });
 });
