bfd52a0c8184f51796585526958961e25074b915
Dien Luong <dysonlu@gmail.com>
2017-10-21 16:22:55 -0400

    Minor changes

--- src/mv-parser.js
+++ src/mv-parser.js
@@ -1,8 +1,8 @@
 //http://stackoverflow.com/a/25580289
-var glob = require('glob');
+const glob = require('glob');
 
 function createParser () {
-    const opts = {
+    const options = {
         nobrace: true,
         noglobstar: true,
         noext: true,
@@ -10,10 +10,15 @@ function createParser () {
     };
 
     return {
-        resolve: function (patterns) {
-            //returns array of string for the matches, or empty array if no match
-            var results = glob.sync(patterns, opts);
-            console.log(results);
+        /**
+         * Returns a list of files/paths in the file system, matching the glob pattern.
+         * @method resolve
+         * @param pattern {String} glob pattern
+         * @returns {Array<String>} Files/paths matching the provided pattern
+         */
+        resolve: function (pattern) {
+            //returns array of filenames (string) matching the pattern, or empty array if no match
+            let results = glob.sync(pattern, options);
             return results;
         }
     };
