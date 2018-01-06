26cb3983fcc3f8336bdaac6eb4eaa12ea2aa9096
Dien Luong <dysonlu@gmail.com>
2018-01-06 04:33:51 -0500

    Minor changes

--- src/mv-renamer.js
+++ src/mv-renamer.js
@@ -49,7 +49,7 @@ function createRenamer() {
 
         // Check if destination glob has more * and/or ? wildcards than source glob does...
         if ((dstWildcardsCount.stars > srcWildcardsCount.stars) || (dstWildcardsCount.questions > srcWildcardsCount.questions)) {
-            throw new Error('Invalid glob pattern. Destination glob contains more wildcards than source');
+            throw new Error('Invalid glob pattern. Destination glob contains more wildcards than source. (** is treated as * in source glob.)');
         }
 
         // extract matches for each wildcard (and literal) parts of the source glob
